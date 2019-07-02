15548d7fb407509e52edb41a45a5f59d511d4fdd
burnsky117 <44195620+burnsky117@users.noreply.github.com>
2019-07-02 16:25:34 +0800

    Add files via upload

---
+++ Movie+User - git/Movie.cpp
@@ -0,0 +1,435 @@
+ï»¿#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <cctype>
+#include <cmath>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+const int MaxN = 9000000;
+int nkey, totNode, root;
+map <string, bool> vis;
+char s[MaxN + 500];
+struct TREE {
+	int left, right;
+	string str;
+}tree[MaxN + 500];
+
+void Insert(int nr, string str)
+{
+	if (str == tree[nr].str) return;	
+	else if (str < tree[nr].str) {	
+		if (tree[nr].left == -1) {
+			tree[nr].left = ++totNode;
+			tree[tree[nr].left].str = str;
+			return;
+		}
+		else Insert(tree[nr].left, str);	
+	}
+	else {	
+		if (tree[nr].right == -1) {	
+			tree[nr].right = ++totNode;
+			tree[tree[nr].right].str = str;
+			return;
+		}
+		else Insert(tree[nr].right, str);	
+	}
+}
+
+void BuildTree1()	
+{
+	freopen("keys1.txt", "r", stdin);	
+	scanf("%d", &nkey); totNode = 0; vis.clear(); 
+	for (int i = 0; i <= MaxN; i++) {	
+		tree[i].left = tree[i].right = -1;
+		tree[i].str = "";
+	}
+	root = -1;
+	for (int i = 1; i <= nkey; i++) {
+		string str;
+		cin >> str;		
+		for (int j = 0; j < str.length(); j++)	
+			str[j] = tolower(str[j]);	
+		if (root == -1) {	
+			root = 0;
+			tree[root].str = str;
+		}
+		else Insert(root, str);	
+	}		
+	fclose(stdin);
+}
+
+void BuildTree2()	
+{
+	freopen("keys2.txt", "r", stdin);	
+	scanf("%d", &nkey); totNode = 0; vis.clear(); 
+	for (int i = 0; i <= MaxN; i++) {	
+		tree[i].left = tree[i].right = -1;
+		tree[i].str = "";
+	}
+	root = -1;
+	for (int i = 1; i <= nkey; i++) {
+		string str;
+		cin >> str;		
+		for (int j = 0; j < str.length(); j++)	
+			str[j] = tolower(str[j]);
+		if (root == -1) {
+			root = 0;
+			tree[root].str = str;
+		}
+		else Insert(root, str);	
+	}		
+	fclose(stdin);
+}
+
+void BuildTree3()	
+{
+	freopen("keys3.txt", "r", stdin);	
+	scanf("%d", &nkey); totNode = 0; vis.clear(); 
+	for (int i = 0; i <= MaxN; i++) {	
+		tree[i].left = tree[i].right = -1;
+		tree[i].str = "";
+	}
+	root = -1;
+	for (int i = 1; i <= nkey; i++) {
+		string str;
+		cin >> str;		
+		for (int j = 0; j < str.length(); j++)	
+			str[j] = tolower(str[j]);	
+		if (root == -1) {	
+			root = 0;
+			tree[root].str = str;
+		}
+		else Insert(root, str);	
+	}		
+	fclose(stdin);
+}
+
+void BuildTree4()	
+{
+	freopen("keys4.txt", "r", stdin);	
+	scanf("%d", &nkey); totNode = 0; vis.clear(); 
+	for (int i = 0; i <= MaxN; i++) {	
+		tree[i].left = tree[i].right = -1;
+		tree[i].str = "";
+	}
+	root = -1;
+	for (int i = 1; i <= nkey; i++) {
+		string str;
+		cin >> str;		
+		for (int j = 0; j < str.length(); j++)	
+			str[j] = tolower(str[j]);	
+		if (root == -1) {	
+			root = 0;
+			tree[root].str = str;
+		}
+		else Insert(root, str);	
+	}		
+	fclose(stdin);
+}
+
+void BuildTree5()	
+{
+	freopen("keys5.txt", "r", stdin);	
+	scanf("%d", &nkey); totNode = 0; vis.clear(); 
+	for (int i = 0; i <= MaxN; i++) {	
+		tree[i].left = tree[i].right = -1;
+		tree[i].str = "";
+	}
+	root = -1;
+	for (int i = 1; i <= nkey; i++) {
+		string str;
+		cin >> str;		
+		for (int j = 0; j < str.length(); j++)	
+			str[j] = tolower(str[j]);	
+		if (root == -1) {	
+			root = 0;
+			tree[root].str = str;
+		}
+		else Insert(root, str);	
+	}		
+	fclose(stdin);
+}
+
+
+bool Find(int nr, string str)	
+{
+	if (tree[nr].str == str) return true;	
+	else if (str < tree[nr].str) {	
+		if (tree[nr].left == -1) return false;
+		else return Find(tree[nr].left, str);
+	}
+	else {
+		if (tree[nr].right == -1) return false;
+		else return Find(tree[nr].right, str);
+	}
+}
+
+void Dfs(int root)
+{
+	if (root == -1) return ;
+	Dfs(tree[root].left);
+	cout << tree[root].str << endl;
+	Dfs(tree[root].right);
+}
+
+void Solve()
+{
+	freopen("Words.txt", "r", stdin);
+	int cnt = 0;
+	while (~scanf("%s", s)) {
+		string str = "";
+		for (int i = 0; i < strlen(s); i++) str += s[i];	
+		for (int i = 0; i < strlen(s); i++) str[i] = tolower(str[i]);
+		if (!vis[str] && Find(root, str)) {	
+			cnt++;
+		}
+	}
+	printf("%d\n", cnt);
+	fclose(stdin);
+}
+
+int main()
+{
+	freopen("MovieValue2.txt", "w", stdout);
+	BuildTree2();
+	Solve();
+	fclose(stdout);
+	
+	freopen("MovieValue3.txt", "w", stdout);
+	BuildTree3();
+	Solve();
+	fclose(stdout);
+	
+	freopen("MovieValue4.txt", "w", stdout);
+	BuildTree4();
+	Solve();
+	fclose(stdout);
+	
+	freopen("MovieValue5.txt", "w", stdout);
+	BuildTree5();
+	Solve();
+	fclose(stdout);
+	
+	freopen("MovieValue1.txt", "w", stdout);
+	BuildTree1();
+	Solve();
+	fclose(stdout);	
+
+
+	int a=0;
+	int b=0;
+	int c=0;
+	int d=0;
+	int e=0;
+	
+    freopen("MovieValue1.txt", "r", stdin);
+	scanf("%d", &a);
+    fclose(stdin);
+    	
+    freopen("MovieValue2.txt", "r", stdin);
+	scanf("%d", &b);
+    fclose(stdin);	
+    	
+    freopen("MovieValue3.txt", "r", stdin);
+	scanf("%d", &c);
+    fclose(stdin);	
+    	
+    freopen("MovieValue4.txt", "r", stdin);
+	scanf("%d", &d);
+    fclose(stdin);	
+    	
+    freopen("MovieValue5.txt", "r", stdin);
+	scanf("%d", &e);
+    fclose(stdin);	
+    	
+    int ttt1;
+    int ttt2;
+    
+    if(a>=b&&a>=c&&a>=d&&a>=e){
+    	ttt1=1;
+    	if(b>=c&&b>=d&&b>=e) ttt2=2;
+    	else if(c>=b&&c>=d&&c>=e) ttt2=3;
+    	else if(d>=b&&d>=c&&d>=e) ttt2=4;
+    	else ttt2=5;
+	}
+	
+	else if(b>=a&&b>=c&&b>=d&&b>=e){
+    	ttt1=2;
+    	if(a>=c&&a>=d&&a>=e) ttt2=1;
+    	else if(c>=a&&c>=d&&c>=e) ttt2=3;
+    	else if(d>=a&&d>=c&&d>=e) ttt2=4;
+    	else ttt2=5;
+	}
+	
+	else if(c>=a&&c>=b&&c>=d&&c>=e){
+    	ttt1=3;
+    	if(a>=b&&a>=d&&a>=e) ttt2=1;
+    	else if(b>=a&&b>=d&&b>=e) ttt2=2;
+    	else if(d>=a&&d>=b&&d>=e) ttt2=4;
+    	else ttt2=5;
+	}
+	
+	else if(d>=a&&d>=b&&d>=c&&d>=e){
+    	ttt1=4;
+    	if(a>=b&&a>=c&&a>=e) ttt2=1;
+    	else if(b>=a&&b>=c&&b>=e) ttt2=2;
+    	else if(c>=a&&c>=b&&c>=e) ttt2=3;
+    	else ttt2=5;
+	}
+	
+	else{
+		ttt1=5;
+		if(a>=b&&a>=c&&a>=d) {ttt2=1;}
+    	else if(b>=a&&b>=c&&b>=d) {ttt2=2;} 
+    	else if(c>=a&&c>=b&&c>=d) {ttt2=3;} 
+    	else{ttt2=4;} 
+	}
+	
+		
+	freopen("MovieOverTag.txt", "w", stdout); 
+	printf("%d\n%d\n", ttt1,ttt2);
+	fclose(stdout);
+	
+	if(ttt1==1){
+		if(ttt2==2){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¿Æ»ÃºÍ¶¯×÷----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==3){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¿Æ»ÃºÍ¸ãĞ¦----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);	
+		}
+		else if(ttt2==4){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¿Æ»ÃºÍ¿Ö²À----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==5){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¿Æ»ÃºÍ°®Çé----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+	} 
+	
+	else if(ttt1==2){
+		if(ttt2==1){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¶¯×÷ºÍ¿Æ»Ã----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==3){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¶¯×÷ºÍ¸ãĞ¦----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==4){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¶¯×÷ºÍ¿Ö²À----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);	
+		}
+		else if(ttt2==5){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¶¯×÷ºÍ°®Çé----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+	}
+	
+	else if(ttt1==3){
+		if(ttt2==1){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¸ãĞ¦ºÍ¿Æ»Ã----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==2){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¸ãĞ¦ºÍ¶¯×÷----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==4){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¸ãĞ¦ºÍ¿Ö²À----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==5){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¸ãĞ¦ºÍ°®Çé----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+	}
+	
+	else if(ttt1==4){
+		if(ttt2==1){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¿Ö²ÀºÍ¿Æ»Ã----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==2){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¿Ö²ÀºÍ¶¯×÷----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==3){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¿Ö²ÀºÍ¸ãĞ¦----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==5){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º¿Ö²ÀºÍ°®Çé----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+	}
+
+	else if(ttt1==5){
+		if(ttt2==1){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º°®ÇéºÍ¿Æ»Ã----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==2){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º°®ÇéºÍ¶¯×÷----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==3){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º°®ÇéºÍ¸ãĞ¦----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+		else if(ttt2==4){
+			string ttt3 ;
+			freopen("£¡£¡£¡MovieOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("¾­ÅĞ¶Ï£¬´ËµçÓ°Í»³öÊôĞÔÀà±ğÎª£º°®ÇéºÍ¿Ö²À----ÒÑÂ¼ÈëµçÓ°·ÖÀà¿â");
+			fclose(stdout);
+		}
+	}
+	 
+	 
+
+	return 0;
+}
---
+++ Movie+User - git/User.cpp
@@ -0,0 +1,447 @@
+ï»¿#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <cctype>
+#include <cmath>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+const int MaxN = 1000;
+int nkey, totNode, root;
+map <string, bool> vis;
+char s[MaxN + 5];
+struct TREE {
+	int left, right;
+	string str;
+}tree[MaxN + 5];
+
+void Insert(int nr, string str)
+{
+	if (str == tree[nr].str) return;
+	else if (str < tree[nr].str) {	
+		if (tree[nr].left == -1) {	
+			tree[nr].left = ++totNode;
+			tree[tree[nr].left].str = str;
+			return;
+		}
+		else Insert(tree[nr].left, str);	
+	}
+	else {	
+		if (tree[nr].right == -1) {	
+			tree[nr].right = ++totNode;
+			tree[tree[nr].right].str = str;
+			return;
+		}
+		else Insert(tree[nr].right, str);
+	}
+}
+
+void BuildTree1()	
+{
+	freopen("keys1.txt", "r", stdin);	
+	scanf("%d", &nkey); totNode = 0; vis.clear(); 
+	for (int i = 0; i <= MaxN; i++) {
+		tree[i].left = tree[i].right = -1;
+		tree[i].str = "";
+	}
+	root = -1;
+	for (int i = 1; i <= nkey; i++) {
+		string str;
+		cin >> str;		
+		for (int j = 0; j < str.length(); j++)	
+			str[j] = tolower(str[j]);
+		if (root == -1) {	
+			root = 0;
+			tree[root].str = str;
+		}
+		else Insert(root, str);	
+	}		
+	fclose(stdin);
+}
+
+void BuildTree2()
+{
+	freopen("keys2.txt", "r", stdin);	
+	scanf("%d", &nkey); totNode = 0; vis.clear(); 
+	for (int i = 0; i <= MaxN; i++) {	
+		tree[i].left = tree[i].right = -1;
+		tree[i].str = "";
+	}
+	root = -1;
+	for (int i = 1; i <= nkey; i++) {
+		string str;
+		cin >> str;		
+		for (int j = 0; j < str.length(); j++)	
+			str[j] = tolower(str[j]);
+		if (root == -1) {	
+			root = 0;
+			tree[root].str = str;
+		}
+		else Insert(root, str);	
+	}		
+	fclose(stdin);
+}
+
+void BuildTree3()	
+{
+	freopen("keys3.txt", "r", stdin);	
+	scanf("%d", &nkey); totNode = 0; vis.clear(); 
+	for (int i = 0; i <= MaxN; i++) {	
+		tree[i].left = tree[i].right = -1;
+		tree[i].str = "";
+	}
+	root = -1;
+	for (int i = 1; i <= nkey; i++) {
+		string str;
+		cin >> str;	
+		for (int j = 0; j < str.length(); j++)
+			str[j] = tolower(str[j]);	
+		if (root == -1) {
+			root = 0;
+			tree[root].str = str;
+		}
+		else Insert(root, str);	
+	}		
+	fclose(stdin);
+}
+
+void BuildTree4()	
+{
+	freopen("keys4.txt", "r", stdin);
+	scanf("%d", &nkey); totNode = 0; vis.clear(); 
+	for (int i = 0; i <= MaxN; i++) {
+		tree[i].left = tree[i].right = -1;
+		tree[i].str = "";
+	}
+	root = -1;
+	for (int i = 1; i <= nkey; i++) {
+		string str;
+		cin >> str;		
+		for (int j = 0; j < str.length(); j++)	
+			str[j] = tolower(str[j]);
+		if (root == -1) {	
+			root = 0;
+			tree[root].str = str;
+		}
+		else Insert(root, str);	
+	}		
+	fclose(stdin);
+}
+
+void BuildTree5()	
+{
+	freopen("keys5.txt", "r", stdin);	
+	scanf("%d", &nkey); totNode = 0; vis.clear(); 
+	for (int i = 0; i <= MaxN; i++) {	
+		tree[i].left = tree[i].right = -1;
+		tree[i].str = "";
+	}
+	root = -1;
+	for (int i = 1; i <= nkey; i++) {
+		string str;
+		cin >> str;		
+		for (int j = 0; j < str.length(); j++)
+			str[j] = tolower(str[j]);
+		if (root == -1) {	
+			root = 0;
+			tree[root].str = str;
+		}
+		else Insert(root, str);	
+	}		
+	fclose(stdin);
+}
+
+
+
+
+bool Find(int nr, string str)	
+{
+	if (tree[nr].str == str) return true;	
+	else if (str < tree[nr].str) {	
+		if (tree[nr].left == -1) return false;
+		else return Find(tree[nr].left, str);
+	}
+	else {
+		if (tree[nr].right == -1) return false;
+		else return Find(tree[nr].right, str);
+	}
+}
+
+void Dfs(int root)
+{
+	if (root == -1) return ;
+	Dfs(tree[root].left);
+	cout << tree[root].str << endl;
+	Dfs(tree[root].right);
+}
+
+void Solve()
+{
+	freopen("Words.txt", "r", stdin);
+	int cnt = 0;
+	while (~scanf("%s", s)) {
+		string str = "";
+		for (int i = 0; i < strlen(s); i++) str += s[i];
+		for (int i = 0; i < strlen(s); i++) str[i] = tolower(str[i]);
+		if (!vis[str] && Find(root, str)) {	
+			cnt++;	
+		}
+	}
+	printf("%d\n", cnt);
+	fclose(stdin);
+}
+
+int main()
+{
+
+	freopen("UserValue2.txt", "w", stdout);
+	BuildTree2();
+	Solve();
+	fclose(stdout);
+	
+	freopen("UserValue3.txt", "w", stdout);
+	BuildTree3();
+	Solve();
+	fclose(stdout);
+	
+	freopen("UserValue4.txt", "w", stdout);
+	BuildTree4();
+	Solve();
+	fclose(stdout);
+	
+	freopen("UserValue5.txt", "w", stdout);
+	BuildTree5();
+	Solve();
+	fclose(stdout);
+	
+	freopen("UserValue1.txt", "w", stdout);
+	BuildTree1();
+	Solve();
+	fclose(stdout);	
+	
+
+
+	int a=0;
+	int b=0;
+	int c=0;
+	int d=0;
+	int e=0;
+	
+    freopen("UserValue1.txt", "r", stdin);
+	scanf("%d", &a);
+    fclose(stdin);
+    	
+    freopen("UserValue2.txt", "r", stdin);
+	scanf("%d", &b);
+    fclose(stdin);	
+    	
+    freopen("UserValue3.txt", "r", stdin);
+	scanf("%d", &c);
+    fclose(stdin);	
+    	
+    freopen("UserValue4.txt", "r", stdin);
+	scanf("%d", &d);
+    fclose(stdin);	
+    	
+    freopen("UserValue5.txt", "r", stdin);
+	scanf("%d", &e);
+    fclose(stdin);	
+    	
+    int ttt1;
+    int ttt2;
+    
+    if(a>=b&&a>=c&&a>=d&&a>=e){
+    	ttt1=1;
+    	if(b>=c&&b>=d&&b>=e) ttt2=2;
+    	else if(c>=b&&c>=d&&c>=e) ttt2=3;
+    	else if(d>=b&&d>=c&&d>=e) ttt2=4;
+    	else ttt2=5;
+	}
+	
+	else if(b>=a&&b>=c&&b>=d&&b>=e){
+    	ttt1=2;
+    	if(a>=c&&a>=d&&a>=e) ttt2=1;
+    	else if(c>=a&&c>=d&&c>=e) ttt2=3;
+    	else if(d>=a&&d>=c&&d>=e) ttt2=4;
+    	else ttt2=5;
+	}
+	
+	else if(c>=a&&c>=b&&c>=d&&c>=e){
+    	ttt1=3;
+    	if(a>=b&&a>=d&&a>=e) ttt2=1;
+    	else if(b>=a&&b>=d&&b>=e) ttt2=2;
+    	else if(d>=a&&d>=b&&d>=e) ttt2=4;
+    	else ttt2=5;
+	}
+	
+	
+	else if(d>=a&&d>=b&&d>=c&&d>=e){
+    	ttt1=4;
+    	if(a>=b&&a>=c&&a>=e) ttt2=1;
+    	else if(b>=a&&b>=c&&b>=e) ttt2=2;
+    	else if(c>=a&&c>=b&&c>=e) ttt2=3;
+    	else ttt2=5;
+	}
+	
+	else{
+		ttt1=5;
+		if(a>=b&&a>=c&&a>=d) ttt2=1;
+    	else if(b>=a&&b>=c&&b>=e) ttt2=2;
+    	else if(c>=a&&c>=b&&c>=e) ttt2=3;
+    	else ttt2=4;
+	}
+	
+		
+	freopen("UserOverTag.txt", "w", stdout); 
+	printf("%d\n%d\n", ttt1,ttt2);
+	fclose(stdout);
+	
+	
+	
+	
+	
+	
+	
+	if(ttt1==1){
+		if(ttt2==2){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¿Æ»ÃºÍ¶¯×÷	ÎªÄúÍÆ¼ö£º¸ÖÌúÏÀ3¡¢¶¾Òº¡¢ÃÀ¹ú¶Ó³¤3");
+			fclose(stdout);
+		}
+		else if(ttt2==3){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¿Æ»ÃºÍ¸ãĞ¦	ÎªÄúÍÆ¼ö£ºËÀÊÌ¡¢ºÚÒÂÈË3¡¢°Ù±äĞÇ¾ı");
+			fclose(stdout);	
+		}
+		else if(ttt2==4){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¿Æ»ÃºÍ¿Ö²À	ÎªÄúÍÆ¼ö£ºÎÒÊÇ´«Ææ¡¢ÒìĞÎ£ºÆõÔ¼¡¢ºº½­¹ÖÎï");
+			fclose(stdout);
+		}
+		else if(ttt2==5){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¿Æ»ÃºÍ°®Çé	ÎªÄúÍÆ¼ö£º·­ÌìÑ°°®¡¢ĞÇ¼ÊÌØ¹¤£ºÇ§ĞÇÖ®³Ç¡¢»úÆ÷ÈË×Ü¶¯Ô±");
+			fclose(stdout);
+		}
+	} 
+	
+	else if(ttt1==2){
+		if(ttt2==1){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¶¯×÷ºÍ¿Æ»Ã	ÎªÄúÍÆ¼ö£ºº£Íõ¡¢Í·ºÅÍæ¼Ò¡¢xÕ½¾¯");
+			fclose(stdout);
+		}
+		else if(ttt2==3){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¶¯×÷ºÍ¸ãĞ¦	ÎªÄúÍÆ¼ö£º´ó±øĞ¡½«¡¢×íÈ­¡¢ÁÚ¼ÒÌØ¹¤");
+			fclose(stdout);
+		}
+		else if(ttt2==4){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¶¯×÷ºÍ¿Ö²À	ÎªÄúÍÆ¼ö£º¸çË¹À­2¡¢ºÚ°µ´«Ëµ5¡¢Ä¾ÄËÒÁ3");
+			fclose(stdout);	
+		}
+		else if(ttt2==5){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¶¯×÷ºÍ°®Çé	ÎªÄúÍÆ¼ö£ºÊ·ÃÜË¹·ò¸¾¡¢ÌÓÑ§ÍşÁú¡¢×³Ö¾ÁèÔÆ");
+			fclose(stdout);
+		}
+	}
+	
+	else if(ttt1==3){
+		if(ttt2==1){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¸ãĞ¦ºÍ¿Æ»Ã	ÎªÄúÍÆ¼ö£º·è¿ñµÄÍâĞÇÈË£¬³¤½­ÆßºÅ¡¢»Øµ½Î´À´");
+			fclose(stdout);
+		}
+		else if(ttt2==2){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¸ãĞ¦ºÍ¶¯×÷	ÎªÄúÍÆ¼ö£º·É³ÛÈËÉú¡¢ÌÆÈË½ÖÌ½°¸¡¢ÍõÅÆÌØ¹¤2");
+			fclose(stdout);
+		}
+		else if(ttt2==4){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¸ãĞ¦ºÍ¿Ö²À	ÎªÄúÍÆ¼ö£ºÈË¼äÊ§¸ñ¡¢ĞÂ¿ªĞÄ¹í¡¢¿ªĞÄ¹í´³½­ºş");
+			fclose(stdout);
+		}
+		else if(ttt2==5){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¸ãĞ¦ºÍ°®Çé	ÎªÄúÍÆ¼ö£ºÎ÷ºìÊĞÊ×¸»¡¢´ó»°Î÷ÓÎ¡¢Ï²¾çÖ®Íõ");
+			fclose(stdout);
+		}
+	}
+	
+	else if(ttt1==4){
+		if(ttt2==1){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¿Ö²ÀºÍ¿Æ»Ã	ÎªÄúÍÆ¼ö£ºÒìĞÔ2¡¢¼ÄÉúÊŞ¡¢¶ñÀÇ");
+			fclose(stdout);
+		}
+		else if(ttt2==2){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¿Ö²ÀºÍ¶¯×÷	ÎªÄúÍÆ¼ö£ºÉÁÁéÕ½Ê¿¡¢»îËÀÈËÀèÃ÷¡¢ÈËÍæ¹í");
+			fclose(stdout);
+		}
+		else if(ttt2==3){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¿Ö²ÀºÍ¸ãĞ¦	ÎªÄúÍÆ¼ö£º»Ø»êÒ¹¡¢Ò»Ã¼µÀÈË¡¢½©Ê¬¼Ò×å");
+			fclose(stdout);
+		}
+		else if(ttt2==5){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º¿Ö²ÀºÍ°®Çé	ÎªÄúÍÆ¼ö£ºÎÒµÄÉ¥Ê¬Å®ÓÑ¡¢ÈË¹íÇéÎ´ÁË¡¢Ù»Å®ÓÄ»ê3");
+			fclose(stdout);
+		}
+	}
+
+	else if(ttt1==5){
+		if(ttt2==1){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º°®ÇéºÍ¿Æ»Ã	ÎªÄúÍÆ¼ö£ºÄãµÄÃû×Ö¡¢´ó»°Î÷ÓÎÖ®ÔÂ¹â±¦ºĞ¡¢ÃÀÈËÓã");
+			fclose(stdout);
+		}
+		else if(ttt2==2){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º°®ÇéºÍ¶¯×÷	ÎªÄúÍÆ¼ö£º°×Éß´«¡¢½­É½ÃÀÈË¡¢Çà´ºÈÈÑª");
+			fclose(stdout);
+		}
+		else if(ttt2==3){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º°®ÇéºÍ¸ãĞ¦	ÎªÄúÍÆ¼ö£ºÌÆ²®»¢µãÇïÏã¡¢ÄÇĞ©Äê£¬ÎÒÃÇÒ»Æğ×·¹ıµÄÅ®º¢¡¢");
+			fclose(stdout);
+		}
+		else if(ttt2==4){
+			string ttt3 ;
+			freopen("£¡£¡£¡UserOverTag£¡£¡£¡.txt", "w", stdout); 
+			printf("Äú×îºÏÊÊµÄµçÓ°ÀàĞÍÎª£º°®ÇéºÍ¿Ö²À	ÎªÄúÍÆ¼ö£ºÙ»Å®ÓÄ»ê¡¢ËûÎªÎÒÍÊÈ¥Æ¤·ô¡¢ÈË¹íÇéÉñ");
+			fclose(stdout);
+		}
+	}
+	 
+	 
+	
+	
+	return 0;
+}
